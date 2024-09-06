8d87f9ab1a6405c9b847ef3c2d987adf047b7d37
AugustExner <exneraugust@gmail.com>
2024-09-06 15:14:01 +0200

    Merging with main

---
+++ app/src/main/java/com/example/carparking/components/Buttons/FindParkingButton.kt
@@ -0,0 +1,26 @@
+package com.example.carparking.Components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun FindParking(text: String) {
+    Button(
+        onClick = { printTextContent(text) },  // Call the method with the current text
+        shape = RoundedCornerShape(16.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .height(60.dp),
+    ) {
+        Text("Find parking!", fontSize = 24.sp)
+    }
+}
