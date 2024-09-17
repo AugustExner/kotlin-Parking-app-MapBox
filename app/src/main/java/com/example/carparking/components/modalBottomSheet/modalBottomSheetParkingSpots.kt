a8d22fd5ce14b37aa6b574e0446d31601ded1c28
AugustExner <exneraugust@gmail.com>
2024-09-17 10:28:25 +0200

    Sorted by available parking spots

--- app/src/main/java/com/example/carparking/components/modalBottomSheet/modalBottomSheetParkingSpots.kt
+++ app/src/main/java/com/example/carparking/components/modalBottomSheet/modalBottomSheetParkingSpots.kt
@@ -4,8 +4,13 @@ import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
@@ -22,27 +27,53 @@ fun ModalBottomSheetParkingSpots(
     onDismissRequest: () -> Unit,
     parkingViewModel: ParkingViewModel = viewModel()
 ) {
-    val parkingSpots = parkingViewModel.parkingSpots
+    val parkingSpots = parkingViewModel.parkingSpots.sortedByDescending { it.ledigePladser }
+
     ModalBottomSheet(
         modifier = Modifier.fillMaxHeight(),
         sheetState = sheetState,
         onDismissRequest = onDismissRequest
     ) {
-        Column(modifier = Modifier.padding(16.dp)) {
+        val scrollState = rememberScrollState()
+
+
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+                .verticalScroll(scrollState) // Add verticalScroll modifier
+        ) {
             Text(
                 text = "Swipe up to open sheet. Swipe down to dismiss."
             )
 
-
-            Log.v("parkingSpots", "ParkingSpots: $parkingSpots")
             parkingSpots.map {
-                Text(
-                    text = it.parkeringsplads
-                )
+                Card(
+                    shape = RoundedCornerShape(8.dp),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                ) {
+                    Column(modifier = Modifier.padding(8.dp)) { // Wrap the Text in a Column for padding
+                        Text(
+                            text = it.parkeringsplads,
+                        )
+                        Text(
+                            text = "Frie Pladser: " + it.ledigePladser.toString(),
+                        )
+                        Text(
+                            text = "Antal Pladser: " + it.antalPladser.toString(),
+                        )
+                    }
+                }
                 Spacer(modifier = Modifier.height(8.dp)) // Add space between cards
             }
-
         }
+
     }
 
 }
+
+
+
+
+
