70736792c0da1b02ce07e9dee438461cd459b51b
AugustExner <exneraugust@gmail.com>
2024-09-19 20:28:42 +0200

    Opening Google maps with navigation has been included

--- app/src/main/java/com/example/carparking/components1/modalBottomSheet/modalBottomSheetParkingSpots.kt
+++ app/src/main/java/com/example/carparking/components1/modalBottomSheet/modalBottomSheetParkingSpots.kt
@@ -2,6 +2,7 @@ package com.example.carparking.components1.modalBottomSheet
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
@@ -11,130 +12,44 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.core.graphics.toColorInt
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.carparking.R
+import com.example.carparking.components1.parkingoverview.ParkingOverview
 import com.example.carparking.components1.parkingoverview.ParkingViewModel
 import com.example.carparking.components1.parkingoverview.directionsAPI.makeApiCallTestWithOriginAndDestinationParameter
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ModalBottomSheetParkingSpots(
-    sheetState: androidx.compose.material3.SheetState,
+    sheetState: SheetState,
     onDismissRequest: () -> Unit,
     parkingViewModel: ParkingViewModel = viewModel(),
     searchQuery: String,
 ) {
     val parkingSpots = parkingViewModel.parkingSpots.sortedByDescending { it.ledigePladser }
-    val scrollState = rememberScrollState()
 
     ModalBottomSheet(
         modifier = Modifier.fillMaxHeight(),
         sheetState = sheetState,
         onDismissRequest = onDismissRequest,
-        tonalElevation = 8.dp,  // Add slight elevation for modern look
+        tonalElevation = 8.dp
     ) {
         Column(
             modifier = Modifier
                 .padding(16.dp)
-                .verticalScroll(scrollState)
+                .verticalScroll(rememberScrollState())
         ) {
-            Text(
-                text = "Available Parking Spots",
-                fontSize = 22.sp,
-                fontWeight = FontWeight.Bold,
-                modifier = Modifier.padding(bottom = 16.dp)
-            )
 
-            parkingSpots.map {
-                val remainingDistance = remember { mutableIntStateOf(0) }
 
-                makeApiCallTestWithOriginAndDestinationParameter(
-                    it.latitude,
-                    it.longitude,
-                    searchQuery
-                ) { distance ->
-                    remainingDistance.value = distance
-                }
-
-                //Card Design
-                Card(
-                    shape = RoundedCornerShape(16.dp),
-                    colors = CardDefaults.cardColors(
-                        containerColor = Color(0xFFECEFF1)  // Light grey background for the card
-                    ),
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(vertical = 8.dp),
-                    elevation = CardDefaults.cardElevation(4.dp)  // Slight elevation for shadow
-                ) {
-                    Column(
-                        modifier = Modifier
-                            .padding(16.dp)
-                            .fillMaxWidth()
-                    ) {
-                        // Row for Destination and Distance
-                        Row(
-                            horizontalArrangement = Arrangement.SpaceBetween,
-                            modifier = Modifier.fillMaxWidth()
-                        ) {
-                            // Icon for Destination
-                            Row {
-                                Icon(
-                                    imageVector = Icons.Filled.LocationOn,
-                                    contentDescription = "Location Icon",
-                                    tint = Color(0xFF0D47A1),  // Blue tint for icon
-                                    modifier = Modifier.size(24.dp)
-                                )
-                                Spacer(modifier = Modifier.width(8.dp))
-                                Text(
-                                    text = searchQuery.replaceFirstChar { it.uppercase() },
-                                    fontSize = 16.sp,
-                                    fontWeight = FontWeight.SemiBold
-                                )
-                            }
-                            Text(
-                                text = "${remainingDistance.value} m",
-                                fontSize = 14.sp,
-                                color = Color.Gray
-                            )
-                        }
-
-                        Spacer(modifier = Modifier.height(12.dp))
-
-                        Row(
-                            horizontalArrangement = Arrangement.SpaceBetween,
-                            modifier = Modifier.fillMaxWidth()
-
-                        ) {
-                            // Image for parking
-                            Row {
-                                Image(painter = painterResource(
-                                    id = R.drawable.parking_icon),
-                                    contentDescription = "Parking Icon",
-                                    modifier = Modifier.size(20.dp)
-                                )
-                                Spacer(modifier = Modifier.width(12.dp))
-                                Text(
-                                    text = it.parkeringsplads,
-                                    fontSize = 16.sp,
-                                    fontWeight = FontWeight.SemiBold
-                                )
-                            }
-                            Text(
-                                text =it.ledigePladser.toString() +"/"+ it.antalPladser.toString(),
-                                fontSize = 14.sp,
-                                color = Color.Black
-                            )
-                        }
-                    }
-                }
+            parkingSpots.forEach { parkingSpot ->
+                ParkingSpotItem(spot = parkingSpot, searchQuery = searchQuery)
             }
         }
     }
 }
+
+
--- app/src/main/java/com/example/carparking/components1/modalBottomSheet/parkingSpotItems.kt
+++ app/src/main/java/com/example/carparking/components1/modalBottomSheet/parkingSpotItems.kt
@@ -1,2 +1,126 @@
 package com.example.carparking.components1.modalBottomSheet
 
+import android.content.Intent
+import android.net.Uri
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.carparking.R
+import com.example.carparking.components1.parkingoverview.ParkingOverview
+import com.example.carparking.components1.parkingoverview.directionsAPI.makeApiCallTestWithOriginAndDestinationParameter
+
+
+@Composable
+fun ParkingSpotItem(spot: ParkingOverview, searchQuery: String) {
+    val remainingDistance = remember { mutableIntStateOf(0) }
+    val context = LocalContext.current
+
+    makeApiCallTestWithOriginAndDestinationParameter(
+        spot.latitude, spot.longitude, searchQuery
+    ) { distance ->
+        remainingDistance.value = distance
+    }
+    Card(
+        shape = RoundedCornerShape(16.dp),
+        colors = CardDefaults.cardColors(containerColor = Color(0xFFECEFF1)),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp),
+        elevation = CardDefaults.cardElevation(4.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+                .clickable {
+                    println("Clicked on ${spot.parkeringsplads}")
+                    // Google Maps navigation URI
+                    val gmmIntentUri = Uri.parse("google.navigation:q=${spot.latitude},${spot.longitude}&mode=d")
+                    // Create an Intent to open Google Maps in navigation mode
+                    val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri).apply {
+                        setPackage("com.google.android.apps.maps")
+                    }
+                    // Start the activity if Google Maps is available
+                    if (mapIntent.resolveActivity(context.packageManager) != null) {
+                        context.startActivity(mapIntent)
+                    }
+                }
+        ) {
+            // Distance row
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxWidth()
+            ) {
+                Row {
+                    Icon(
+                        imageVector = Icons.Filled.LocationOn,
+                        contentDescription = "Location Icon",
+                        tint = Color(0xFF0D47A1),
+                        modifier = Modifier.size(24.dp)
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Text(
+                        text = searchQuery.replaceFirstChar { it.uppercase() },
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.SemiBold
+                    )
+                }
+                Text(
+                    text = "${remainingDistance.value} m", fontSize = 14.sp, color = Color.Gray
+                )
+            }
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            // Parking spot info
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxWidth()
+            ) {
+                Row {
+                    Image(
+                        painter = painterResource(id = R.drawable.parking_icon),
+                        contentDescription = "Parking Icon",
+                        modifier = Modifier.size(20.dp)
+                    )
+                    Spacer(modifier = Modifier.width(12.dp))
+                    Text(
+                        text = spot.parkeringsplads,
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.SemiBold
+                    )
+                }
+                Text(
+                    text = "Fri :${spot.ledigePladser}/${spot.antalPladser}",
+                    fontSize = 14.sp,
+                    color = Color.Black
+                )
+            }
+        }
+    }
+}
